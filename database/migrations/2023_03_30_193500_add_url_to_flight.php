071ecf543a3d3abe0daecd8ef605e9e3b5864d51
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2023-03-30 20:40:55 +0200

    .

--- database/migrations/2023_03_30_193500_add_url_to_flight.php
+++ database/migrations/2023_03_30_193500_add_url_to_flight.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ChangeAirportDefaultView extends Migration
+class AddUrlToFlight extends Migration
 {
     /**
      * Run the migrations.
